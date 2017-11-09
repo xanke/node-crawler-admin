a135bb4b190c5ca802ee4beaed843e0bcb18296a
xank <xank@qq.com>
2017-11-09 18:34:29 +0800

    port 9099

--- config/index.js
+++ config/index.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 9090,
+    port: 9099,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
