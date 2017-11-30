1d277be4c78d133d629a8496ef757d0a78986302
xank <xank@qq.com>
2017-12-01 00:49:10 +0800

    修改密码bug

--- src/components/Account/changePwd.vue
+++ src/components/Account/changePwd.vue
@@ -1,82 +1,78 @@
 <template>
 	<el-dialog ref="dialog" :visible.sync="visible" title="修改密码">
-	 
     <el-form ref="form" :model="form" :rules="rules" label-width="80px">
       <el-form-item label="旧密码" prop="old_pwd">
-        <el-input v-model.trim="form.old_pwd"></el-input>
+        <el-input type="password" v-model.trim="form.old_pwd"></el-input>
       </el-form-item>
       <el-form-item label="新密码" prop="new_pwd">
-        <el-input v-model.trim="form.new_pwd"></el-input>
+        <el-input type="password" v-model.trim="form.new_pwd"></el-input>
       </el-form-item>
     </el-form>
- 
 		<div slot="footer" class="dialog-footer">
-			<el-button type="primary" class="fl m-l-20" :disabled="disable" @click="submit()">提交</el-button>
+      <el-button type="default"  @click="visible = false">取消</el-button>
+			<el-button type="primary"  :disabled="disable" @click="submit()">提交</el-button>
 		</div>
 	</el-dialog>
 </template>
-<style>
-
-</style>
 <script>
-  import http from '@/assets/js/http'
-
-  export default {
-    data() {
-      return {
-        visible: false,
-        disable: false,
-        form: {
-          auth_key: '',
-          old_pwd: '',
-          new_pwd: ''
-        },
-        rules: {
-          old_pwd: [
-            { required: true, message: '请输入旧密码', trigger: 'blur' },
-            { min: 6, max: 12, message: '长度在 6 到 12 个字符', trigger: 'blur' }
-          ],
-          new_pwd: [
-            { required: true, message: '请输入新密码', trigger: 'blur' },
-            { min: 6, max: 12, message: '长度在 6 到 12 个字符', trigger: 'blur' }
-          ]
-        }
+import http from "@/assets/js/http"
+export default {
+  data() {
+    return {
+      visible: false,
+      disable: false,
+      form: {
+        auth_key: "",
+        old_pwd: "",
+        new_pwd: ""
+      },
+      rules: {
+        old_pwd: [
+          { required: true, message: "请输入旧密码", trigger: "blur" },
+          { min: 6, max: 12, message: "长度在 6 到 12 个字符", trigger: "blur" }
+        ],
+        new_pwd: [
+          { required: true, message: "请输入新密码", trigger: "blur" },
+          { min: 6, max: 12, message: "长度在 6 到 12 个字符", trigger: "blur" }
+        ]
       }
+    }
+  },
+  methods: {
+    open() {
+      this.visible = true
     },
-    methods: {
-      open() {
-        console.log('s')
-        this.visible = true
-        // this.$refs.dialog.open()
-      },
-      close() {
-        this.visible = false
-        // this.$refs.dialog.close()
-      },
-      submit() {
-        this.$refs.form.validate((pass) => {
-          if (pass) {
-            this.disable = !this.disable
-            this.apiPost('admin/base/setInfo', this.form).then((res) => {
-              this.handelResponse(res, (data) => {
-                _g.toastMsg('success', '修改成功')
-                Lockr.rm('authKey')
-                Lockr.rm('authList')
-                Lockr.rm('sessionId')
+    close() {
+      this.visible = false
+    },
+    submit() {
+      this.$refs.form.validate(pass => {
+        if (pass) {
+          this.disable = !this.disable
+          this.apiPost("admin/base/setInfo", this.form).then(res => {
+            this.handelResponse(
+              res,
+              data => {
+                _g.toastMsg("success", "修改成功")
+                Lockr.rm("authKey")
+                Lockr.rm("authList")
+                Lockr.rm("sessionId")
                 setTimeout(() => {
-                  router.replace('/')
+                  router.replace("/")
                 }, 1500)
-              }, () => {
+              },
+              () => {
                 this.disable = !this.disable
-              })
-            })
-          }
-        })
-      }
-    },
-    created() {
-      this.form.auth_key = Lockr.get('authKey')
-    },
-    mixins: [http]
-  }
+              }
+            )
+          })
+        }
+      })
+    }
+  },
+  created() {
+    this.form.auth_key = Lockr.get("authKey")
+  },
+  mixins: [http]
+}
 </script>
