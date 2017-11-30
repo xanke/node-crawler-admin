92387915cb453c0e2ddeba463c33cdbe0ba63e41
xank <xank@qq.com>
2017-12-01 00:55:41 +0800

    去左侧栏

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,7 +10,7 @@
         </template>
 			</el-col>
 			<el-col :span="16" class="ofv-hd">
-				<div class="fl p-l-20 p-r-20 top-menu" :class="{'top-active': menu.selected}" v-for="menu in topMenu" @click="switchTopMenu(menu)">{{menu.title}}</div>
+				<div class="fl p-l-20 p-r-20 top-menu" :key="index" :class="{'top-active': menu.selected}" v-for="(menu, index) in topMenu" @click="switchTopMenu(menu)">{{menu.title}}</div>
 			</el-col>
 			<el-col :span="4" class="pos-rel">
 				<el-dropdown @command="handleMenu" class="user-menu">
@@ -25,9 +25,9 @@
 			</el-col>
 		</el-col>
 		<el-col :span="24" class="panel-center">
-			<aside class="w-180 ovf-hd" v-show="!showLeftMenu">
+			<!-- <aside class="w-180 ovf-hd" v-show="!showLeftMenu">
 				<leftMenu :menuData="menuData" :menu="menu" ref="leftMenu"></leftMenu>
-			</aside>
+			</aside> -->
 			<section class="panel-c-c" :class="{'hide-leftMenu': hasChildMenu}">
 				<div class="grid-content bg-purple-light">
 					<el-col :span="24">
@@ -39,220 +39,219 @@
 			</section>
 		</el-col>
 		<changePwd ref="changePwd"></changePwd>
-
 	</el-row>
 </template>
 <style>
-	.fade-enter-active,
-	.fade-leave-active {
-		transition: opacity .5s
-	}
-	
-	.fade-enter,
-	.fade-leave-active {
-		opacity: 0
-	}
-	
-	.panel {
-		position: absolute;
-		top: 0px;
-		bottom: 0px;
-		width: 100%;
-	}
-	
-	.panel-top {
-		height: 60px;
-		line-height: 60px;
-		background: #1F2D3D;
-		color: #c0ccda;
-	}
-	
-	.panel-center {
-		background: #324057;
-		position: absolute;
-		top: 60px;
-		bottom: 0px;
-		overflow: hidden;
-	}
-	
-	.panel-c-c {
-		background: #f1f2f7;
-		position: absolute;
-		right: 0px;
-		top: 0px;
-		bottom: 0px;
-		left: 180px;
-		overflow-y: scroll;
-		padding: 20px;
-	}
-	
-	.logout {
-		background: url(../assets/images/logout_36.png);
-		background-size: contain;
-		width: 20px;
-		height: 20px;
-		float: left;
-	}
-	
-	.logo {
-		width: 150px;
-		float: left;
-		margin: 10px 10px 10px 18px;
-	}
-	
-	.tip-logout {
-		float: right;
-		margin-right: 20px;
-		padding-top: 5px;
-		cursor: pointer;
-	}
-	
-	.admin {
-		color: #c0ccda;
-		text-align: center;
-	}
-	.hide-leftMenu {
-		left: 0px;
-	}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
+
+.panel {
+  position: absolute;
+  top: 0px;
+  bottom: 0px;
+  width: 100%;
+}
+
+.panel-top {
+  height: 60px;
+  line-height: 60px;
+  background: #1f2d3d;
+  color: #c0ccda;
+}
+
+.panel-center {
+  background: #324057;
+  position: absolute;
+  top: 60px;
+  bottom: 0px;
+  overflow: hidden;
+}
+
+.panel-c-c {
+  background: #f1f2f7;
+  position: absolute;
+  right: 0px;
+  top: 0px;
+  bottom: 0px;
+  left: 0px;
+  overflow-y: scroll;
+  padding: 20px;
+}
+
+.logout {
+  background: url(../assets/images/logout_36.png);
+  background-size: contain;
+  width: 20px;
+  height: 20px;
+  float: left;
+}
+
+.logo {
+  width: 150px;
+  float: left;
+  margin: 10px 10px 10px 18px;
+}
+
+.tip-logout {
+  float: right;
+  margin-right: 20px;
+  padding-top: 5px;
+  cursor: pointer;
+}
+
+.admin {
+  color: #c0ccda;
+  text-align: center;
+}
+.hide-leftMenu {
+  left: 0px;
+}
 </style>
 <script>
-  import leftMenu from '@/components/Common/leftMenu.vue'
-  import changePwd from '@/components/Account/changePwd.vue'
-  import http from '@/assets/js/http'
+import leftMenu from "@/components/Common/leftMenu.vue";
+import changePwd from "@/components/Account/changePwd.vue";
+import http from "@/assets/js/http";
 
-  export default {
-    data() {
-      return {
-        username: '',
-        topMenu: [],
-        childMenu: [],
-        menuData: [],
-        hasChildMenu: false,
-        menu: null,
-        module: null,
-        img: '',
-        title: '',
-        logo_type: null
-      }
-    },
-    methods: {
-      logout() {
-        this.$confirm('确认退出吗?', '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消'
-        }).then(() => {
-          _g.openGlobalLoading()
-          let data = {
-            authkey: Lockr.get('authKey'),
-            sessionId: Lockr.get('sessionId')
-          }
-          this.apiPost('admin/base/logout', data).then((res) => {
-            _g.closeGlobalLoading()
-            this.handelResponse(res, (data) => {
-              Lockr.rm('menus')
-              Lockr.rm('authKey')
-              Lockr.rm('rememberKey')
-              Lockr.rm('authList')
-              Lockr.rm('userInfo')
-              Lockr.rm('sessionId')
-              Cookies.remove('rememberPwd')
-              _g.toastMsg('success', '退出成功')
-              setTimeout(() => {
-                router.replace('/')
-              }, 1500)
-            })
-          })
-        }).catch(() => {
-
-        })
-      },
-      switchTopMenu(item) {
-        if (!item.child) {
-          router.push(item.url)
-        } else {
-          router.push(item.child[0].child[0].url)
-        }
-      },
-      handleMenu(val) {
-        switch (val) {
-          case 'logout':
-            this.logout()
-            break
-          case 'changePwd':
-            this.changePwd()
-            break
-        }
-      },
-      changePwd() {
-        this.$refs.changePwd.open()
-      },
-      getTitleAndLogo() {
-        this.apiPost('admin/base/getConfigs').then((res) => {
-          this.handelResponse(res, (data) => {
-            document.title = data.SYSTEM_NAME
-            this.logo_type = data.LOGO_TYPE
-            this.title = data.SYSTEM_NAME
-            this.img = window.HOST + data.SYSTEM_LOGO
-          })
-        })
-      },
-      getUsername() {
-        this.username = Lockr.get('userInfo').username
-      }
-    },
-    created() {
-      this.getTitleAndLogo()
-      let authKey = Lockr.get('authKey')
-      let sessionId = Lockr.get('sessionId')
-      if (!authKey || !sessionId) {
-        _g.toastMsg('warning', '您尚未登录')
-        setTimeout(() => {
-          router.replace('/')
-        }, 1500)
-        return
-      }
-      this.getUsername()
-      let menus = Lockr.get('menus')
-      this.menu = this.$route.meta.menu
-      this.module = this.$route.meta.module
-      this.topMenu = menus
-      _(menus).forEach((res) => {
-        if (res.module == this.module) {
-          this.menuData = res.child
-          res.selected = true
-        } else {
-          res.selected = false
-        }
+export default {
+  data() {
+    return {
+      username: "",
+      topMenu: [],
+      childMenu: [],
+      menuData: [],
+      hasChildMenu: false,
+      menu: null,
+      module: null,
+      img: "",
+      title: "",
+      logo_type: null
+    };
+  },
+  methods: {
+    logout() {
+      this.$confirm("确认退出吗?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消"
       })
-    },
-    computed: {
-      routerShow() {
-        return store.state.routerShow
-      },
-      showLeftMenu() {
-        this.hasChildMenu = store.state.showLeftMenu
-        return store.state.showLeftMenu
-      }
-    },
-    components: {
-      leftMenu,
-      changePwd
-    },
-    watch: {
-      '$route' (to, from) {
-        _(this.topMenu).forEach((res) => {
-          if (res.module == to.meta.module) {
-            res.selected = true
-            if (!to.meta.hideLeft) {
-              this.menu = to.meta.menu
-              this.menuData = res.child
-            }
-          } else {
-            res.selected = false
-          }
+        .then(() => {
+          _g.openGlobalLoading();
+          let data = {
+            authkey: Lockr.get("authKey"),
+            sessionId: Lockr.get("sessionId")
+          };
+          this.apiPost("admin/base/logout", data).then(res => {
+            _g.closeGlobalLoading();
+            this.handelResponse(res, data => {
+              Lockr.rm("menus");
+              Lockr.rm("authKey");
+              Lockr.rm("rememberKey");
+              Lockr.rm("authList");
+              Lockr.rm("userInfo");
+              Lockr.rm("sessionId");
+              Cookies.remove("rememberPwd");
+              _g.toastMsg("success", "退出成功");
+              setTimeout(() => {
+                router.replace("/");
+              }, 1500);
+            });
+          });
         })
+        .catch(() => {});
+    },
+    switchTopMenu(item) {
+      if (!item.child) {
+        router.push(item.url);
+      } else {
+        router.push(item.child[0].child[0].url);
       }
     },
-    mixins: [http]
-  }
+    handleMenu(val) {
+      switch (val) {
+        case "logout":
+          this.logout();
+          break;
+        case "changePwd":
+          this.changePwd();
+          break;
+      }
+    },
+    changePwd() {
+      this.$refs.changePwd.open();
+    },
+    getTitleAndLogo() {
+      this.apiPost("admin/base/getConfigs").then(res => {
+        this.handelResponse(res, data => {
+          document.title = data.SYSTEM_NAME;
+          this.logo_type = data.LOGO_TYPE;
+          this.title = data.SYSTEM_NAME;
+          this.img = window.HOST + data.SYSTEM_LOGO;
+        });
+      });
+    },
+    getUsername() {
+      this.username = Lockr.get("userInfo").username;
+    }
+  },
+  created() {
+    this.getTitleAndLogo();
+    let authKey = Lockr.get("authKey");
+    let sessionId = Lockr.get("sessionId");
+    if (!authKey || !sessionId) {
+      _g.toastMsg("warning", "您尚未登录");
+      setTimeout(() => {
+        router.replace("/");
+      }, 1500);
+      return;
+    }
+    this.getUsername();
+    let menus = Lockr.get("menus");
+    this.menu = this.$route.meta.menu;
+    this.module = this.$route.meta.module;
+    this.topMenu = menus;
+    _(menus).forEach(res => {
+      if (res.module == this.module) {
+        this.menuData = res.child;
+        res.selected = true;
+      } else {
+        res.selected = false;
+      }
+    });
+  },
+  computed: {
+    routerShow() {
+      return store.state.routerShow;
+    },
+    showLeftMenu() {
+      this.hasChildMenu = store.state.showLeftMenu;
+      return store.state.showLeftMenu;
+    }
+  },
+  components: {
+    leftMenu,
+    changePwd
+  },
+  watch: {
+    $route(to, from) {
+      _(this.topMenu).forEach(res => {
+        if (res.module == to.meta.module) {
+          res.selected = true;
+          if (!to.meta.hideLeft) {
+            this.menu = to.meta.menu;
+            this.menuData = res.child;
+          }
+        } else {
+          res.selected = false;
+        }
+      });
+    }
+  },
+  mixins: [http]
+};
 </script>
