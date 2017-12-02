78e4113e8709d5f2b4694fad2e4bda8ae73b25e4
xank <xank@qq.com>
2017-12-03 00:28:06 +0800

    format

--- src/components/task/websiteEdit.vue
+++ src/components/task/websiteEdit.vue
@@ -1,34 +1,33 @@
 <template>
-    <el-dialog title="添加网站" :visible.sync="visible">
-      <el-form :model="form" label-width="80px">
-        <el-form-item label="名称">
-          <el-input v-model="form.title" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="网址">
-          <el-input v-model="form.url" auto-complete="off"></el-input>
-        </el-form-item>
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="visible = false">取 消</el-button>
-        <el-button type="primary" @click="submit()">确 定</el-button>
-      </div>
-    </el-dialog>
+  <el-dialog title="添加网站" :visible.sync="visible">
+    <el-form :model="form" label-width="80px">
+      <el-form-item label="名称">
+        <el-input v-model="form.title" auto-complete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="网址">
+        <el-input v-model="form.url" auto-complete="off"></el-input>
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="visible = false">取 消</el-button>
+      <el-button type="primary" @click="submit()">确 定</el-button>
+    </div>
+  </el-dialog>
 </template>
 <script>
-import http from "@/assets/js/http"
+import http from '@/assets/js/http'
 export default {
   data() {
     return {
       visible: false,
       disable: false,
       form: {
-        title: "",
-        url: "",
+        title: '',
+        url: '',
         tid: this.tid
       },
       loading: false,
-      rules: {
-      }
+      rules: {}
     }
   },
   prop() {
@@ -36,7 +35,6 @@ export default {
       tid: [Number]
     }
   },
-
   methods: {
     open() {
       this.visible = true
@@ -45,17 +43,14 @@ export default {
       this.visible = false
     },
     submit() {
-      // this.addWebsiteDialog.form.tid = this.$route.params.id;
-      this.apiPost("website", this.form).then(res => {
+      this.apiPost('website', this.form).then(res => {
         this.handelResponse(res, data => {
-          _g.toastMsg("success", "添加成功");
-          this.visible = false;
+          _g.toastMsg('success', '添加成功')
+          this.visible = false
           // this.getTaskInfo();
-        });
+        })
       })
     }
-  },
-  created() {
   },
   mixins: [http]
 }
